9b2770cf0a5d3e7f72bcc6afd91f5e042abfe397
Ellie <ellie.reid@udig.com>
2021-05-07 15:18:55 -0400

    Update Pom to include resource dependency and added resource class

---
+++ src/main/java/HelloWorldDropwizardResource.java
@@ -0,0 +1,8 @@
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+public class HelloWorldDropwizardResource {
+
+}
