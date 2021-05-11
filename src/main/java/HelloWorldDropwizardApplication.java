786f0c2951179fe07ba94a87ffbefc4b4474384b
Ellie <ellie.reid@udig.com>
2021-05-11 12:21:30 -0400

    Added example class

--- src/main/java/HelloWorldDropwizardApplication.java
+++ src/main/java/HelloWorldDropwizardApplication.java
@@ -1,14 +1,31 @@
+import health.TemplateHealthCheck;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import resources.HelloWorldDropwizardResource;
 
-public class HelloWorldDropwizardApplication extends Application<Configuration> {
+public class HelloWorldDropwizardApplication extends Application<HelloWorldDropwizardConfiguration> {
     public static void main(String[] args) throws Exception {
         new HelloWorldDropwizardApplication().run(args);
     }
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
 
     @Override
-    public void run(Configuration configuration, Environment environment) throws Exception {
-
+    public void initialize(Bootstrap<HelloWorldDropwizardConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+    @Override
+    public void run(HelloWorldDropwizardConfiguration configuration, Environment environment)  {
+        final HelloWorldDropwizardResource resource = new HelloWorldDropwizardResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
     }
 }
--- src/main/java/HelloWorldDropwizardResource.java
+++
@@ -1,8 +0,0 @@
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-public class HelloWorldDropwizardResource {
-
-}
